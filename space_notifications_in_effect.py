be93a0b62a60adf7c4356431fccd7e29373a535c
jusbro <97056196+jusbro@users.noreply.github.com>
2024-05-13 21:16:31 -0400

    Create space_notifications_in_effect.py

---
+++ space_notifications_in_effect.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+# Define the URL for the image
+image_url = "https://services.swpc.noaa.gov/images/notifications-in-effect-timeline.png"
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Define the filename for the image
+image_filename = os.path.join(current_directory, 'swpc_notifications.png')
+
+# Send a GET request to fetch the image
+response = requests.get(image_url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Open a file in binary write mode and save the image
+    with open(image_filename, 'wb') as file:
+        file.write(response.content)
+    print(f"Image successfully saved as {image_filename}")
+else:
+    print(f"Failed to retrieve image. HTTP status code: {response.status_code}")
